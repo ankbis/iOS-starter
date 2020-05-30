898e1aa0474749f850c8053c79a1c28baedd9cf6
Ankush Bisht <ankbis@amazon.com>
2020-05-30 11:33:50 -0400

    UI

--- iOS-Starter/ViewController.swift
+++ iOS-Starter/ViewController.swift
@@ -15,6 +15,30 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func showMessage(sender: UIButton) {
+        
+        // The sender is the button that is tapped by the user.
+        // Here we store the sender in the selectedButton constant
+        let selectedButton = sender
+        
+        // Get the emoji from the title label of the selected button
+        if let wordToLookup = selectedButton.titleLabel?.text {
+            
+            // Get the meaning of the emoji from the dictionary
+            var emojis = ["😀": "laugh",
+                          "😷": "corona",
+                          "🐖":"rahul",
+                          "🎂":"cake",
+                          "🍻":"beer"
+            ]
+            
+            // Change the line below to display the meaning of the emoji instead of Hello World
+            let alertController = UIAlertController(title: "Meaning", message:  emojis[wordToLookup], preferredStyle: UIAlertController.Style.alert)
+            
+            alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+            present(alertController, animated: true, completion: nil)
+        }
+    }
 
 }
 
